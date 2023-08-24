c4b09913dda2906c9ee152d382092d3ef7b8197b
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-24 08:51:20 -0300

    Entendendo o Get e o Query

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -6,7 +6,7 @@ describe('Component: Input', () => {
   it('should render without activity indicator if isLoading is absent', () => {
     render(<Input />);
 
-    const activityIndicator = screen.getByTestId('activity-indicator');
-    console.log(activityIndicator);
+    const activityIndicator = screen.queryByTestId('activity-indicator');
+    expect(activityIndicator).toBeNull();
   });
 });
