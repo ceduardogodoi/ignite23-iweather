f4135bc72bba28cdb07a4530b342b23266e5dd00
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-24 08:46:38 -0300

    testID

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 
 import { Input } from '@components/Input';
 
 describe('Component: Input', () => {
-  it('should render without activity indicator', () => {
-    const { debug } = render(<Input />);
-    debug();
+  it('should render without activity indicator if isLoading is absent', () => {
+    render(<Input />);
+
+    const activityIndicator = screen.getByTestId('activity-indicator');
+    console.log(activityIndicator);
   });
 });
