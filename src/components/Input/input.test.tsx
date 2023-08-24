e6f9943ef55617a25d56d0f9116fd1b6f92259d9
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-24 08:54:06 -0300

    toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -3,10 +3,17 @@ import { render, screen } from '@testing-library/react-native';
 import { Input } from '@components/Input';
 
 describe('Component: Input', () => {
-  it('should render without activity indicator if isLoading is absent', () => {
+  it('should render without activity indicator when isLoading is absent', () => {
     render(<Input />);
 
     const activityIndicator = screen.queryByTestId('activity-indicator');
     expect(activityIndicator).toBeNull();
+  });
+
+  it('should render activity indicator when isLoading is present', () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.queryByTestId('activity-indicator');
+    expect(activityIndicator).toBeTruthy();
   });
 });
