e732193996f0d98f7471c3aafb0eacbc0ac64c16
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-28 11:08:45 -0300

    Pasta para mocks

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,22 +1,12 @@
 import { api } from './api';
 import { getCityByNameService } from './getCityByNameService';
 
+import { mockCityAPIResponse } from '@__tests__/mocks/mockCityApiResponse';
+
 describe('API: getCityByNameService', () => {
   it('should return a city details', async () => {
-    const data = {
-      id: '1',
-      name: 'São Paulo',
-      sys: {
-        country: 'BR',
-      },
-      coord: {
-        lat: 123,
-        lon: 456,
-      },
-    };
-
     jest.spyOn(api, 'get').mockResolvedValue({
-      data,
+      data: mockCityAPIResponse,
     });
 
     const response = await getCityByNameService('São Paulo');
