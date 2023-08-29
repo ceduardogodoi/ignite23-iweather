7a2852eea241aee16742e6cb0ccd66b7c3b677b7
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-29 08:55:48 -0300

    Teste do saveStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,8 +1,23 @@
-import { getStorageCity } from '@libs/asyncStorage/cityStorage';
+import { getStorageCity, saveStorageCity } from '@libs/asyncStorage/cityStorage';
+import { CityProps } from '@services/getCityByNameService';
 
 describe('Storage: cityStorage', () => {
   it('should return null when it does not have a city stored', async () => {
     const response = await getStorageCity();
     expect(response).toBeNull();
+  });
+
+  it('should return the stored city', async () => {
+    const newCity: CityProps = {
+      id: '1',
+      latitude: 123,
+      longitude: 456,
+      name: 'São Paulo',
+    };
+
+    await saveStorageCity(newCity);
+
+    const response = await getStorageCity();
+    expect(response).toEqual(newCity);
   });
 });
