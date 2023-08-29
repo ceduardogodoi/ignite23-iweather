ba1b6d43706109d42153066a9f6d8f61d258001a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-29 08:50:47 -0300

    Mock do Async Storage

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -3,6 +3,6 @@ import { getStorageCity } from '@libs/asyncStorage/cityStorage';
 describe('Storage: cityStorage', () => {
   it('should return null when it does not have a city stored', async () => {
     const response = await getStorageCity();
-    console.log(JSON.stringify(response, null, 2));
+    expect(response).toBeNull();
   });
 });
