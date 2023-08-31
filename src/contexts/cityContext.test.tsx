dc7bd1b1392309b9a12c62ae797011733aab7f2d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-31 07:51:41 -0300

    Wrapper

--- src/contexts/cityContext.test.tsx
+++ src/contexts/cityContext.test.tsx
@@ -1,10 +1,13 @@
 import { renderHook } from '@testing-library/react-native'
 
+import { CityProvider } from '@contexts/CityContext';
 import { useCity } from '@hooks/useCity';
 
 describe('Context: CityContext', () => {
   it('should change the selected city', () => {
-    const { result } = renderHook(() => useCity());
-    console.log(result.current.city);
+    const { result } = renderHook(() => useCity(), {
+      wrapper: CityProvider,
+    });
+    console.log(result.current);
   });
 });
