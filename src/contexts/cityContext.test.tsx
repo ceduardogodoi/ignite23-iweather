7ad03a103d7403b47ae86ddd2cad0d5fd9b21618
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-08-31 07:58:05 -0300

    waitFor e act

--- src/contexts/cityContext.test.tsx
+++ src/contexts/cityContext.test.tsx
@@ -1,13 +1,16 @@
-import { renderHook } from '@testing-library/react-native'
+import { act, renderHook, waitFor } from '@testing-library/react-native'
 
 import { CityProvider } from '@contexts/CityContext';
 import { useCity } from '@hooks/useCity';
 
 describe('Context: CityContext', () => {
-  it('should change the selected city', () => {
+  it('should change the selected city', async () => {
     const { result } = renderHook(() => useCity(), {
       wrapper: CityProvider,
     });
-    console.log(result.current);
+
+    await waitFor(() => act(() => {
+      console.log(result.current);
+    }));
   });
 });
