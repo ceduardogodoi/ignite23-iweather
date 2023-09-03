f67af207499793d05f7a7c7ed9f2fd6de16dd589
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-09-03 10:44:33 -0300

    Testando o Search

---
+++ src/screens/Search/search.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, Providers, screen, waitFor } from '@__tests__/utils/customRender';
+import { Search } from '@screens/Search';
+import { api } from '@services/api';
+import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityAPIResponse';
+
+describe('Screen: Search', () => {
+  it('should show a city option', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockCityAPIResponse });
+
+    render(
+      <Search />
+    );
+
+    const searchInput = screen.getByTestId('search-input');
+    fireEvent.changeText(searchInput, 'São Paulo');
+
+    const option = await waitFor(() => screen.findByText(/são paulo/i));
+    expect(option).toBeTruthy();
+  });
+});
